9ac2f7d55877c2683f5196f4a01daa3f17eed775
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:24:54 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -3,3 +3,11 @@
 #View(dfCleanCensus)
 CensusData<-dfCleanCensus
 ArrestsData <- USArrests
+
+
+
+ArrestsData=cbind(rownames(ArrestsData),ArrestsData) #Combine sequence of dataframe by rows or columns
+#the above function copies the rownames and places it as a column in the existing dataframe 
+colnames(x)=c("stateName") #Giving name to the new column
+View(x)
+
