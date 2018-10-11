18aa2bde352c9b33f8eb09d7ab37bfd3208b7148
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:49:46 -0400

    Saving Scatter.R

--- Scatter.R
+++ Scatter.R
@@ -1,2 +1,6 @@
 
 library(ggplot2)
+
+#Step D: Explore Murders – scatter chart
+# 12)	Generate a scatter plot – have population on the X axis, the percent over 18 on the y axis, and the size & color represent the murder rate
+
