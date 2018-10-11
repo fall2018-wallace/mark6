d4f21589f303eebe9afc8fbbbe987d35aa8b1686
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:56:22 -0400

    Saving Box_Plotter.R

--- Box_Plotter.R
+++ Box_Plotter.R
@@ -1,6 +1,7 @@
 
 #5:Create a boxplot for the population, and a different boxplot for the murder rate.
 library(ggplot2)
-options(scipen=999)ggplot(MergedData,aes(x=factor(0),y=population))+geom_boxplot()
-ggplot(MergedData,aes(x=factor(0),y=Murder))+geom_boxplot()
+options(scipen=999)
+Box_Pop<-ggplot(MergedData,aes(x=factor(0),y=population))+geom_boxplot()
+Box_Mur<-ggplot(MergedData,aes(x=factor(0),y=Murder))+geom_boxplot()
 
