85bfd2503a74d1036aa7835ce3cf4e6d3dc7e4fd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:26:41 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -8,6 +8,6 @@ ArrestsData <- USArrests
 
 ArrestsData=cbind(rownames(ArrestsData),ArrestsData) #Combine sequence of dataframe by rows or columns
 colnames(ArrestsData)=c("stateName") #Giving name to the new column
-View(ArrestsData)
+ArrestsData
 
 #3)	Create a merged dataframe -- with the attributes from both dataframes
