ebda25ecd149fe398aa26e8fee32f282261d6811
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:55:23 -0400

    Saving MurderPlotter.R

--- MurderPlotter.R
+++ MurderPlotter.R
@@ -23,11 +23,11 @@ BarMurdersNew<-ggplot(MergedData,aes(x=stateName,y=TotMurders)) + geom_col() + t
 
 #10:Generate a new bar chart, the same as in the previous step, but also sort the x-axis by the murder rate
 #DataAscending<-MergedData[order(MergedData$TotMurders),]
-BarMurdersAsc<-ggplot(DataAscending,aes(x=reorder(stateName,TotMurders),y=TotMurders)) + geom_col() + theme(axis.text.x=element_text(angle=90,hjust=1)) + ggtitle("Total Murders")
+BarMurdersAsc<-ggplot(MergedData,aes(x=reorder(stateName,TotMurders),y=TotMurders)) + geom_col() + theme(axis.text.x=element_text(angle=90,hjust=1)) + ggtitle("Total Murders")
 
 
 #11:Generate a third bar chart, the same as the previous step, but also showing percentOver18 as the color of the bar
-BarMurdersOvrET<-ggplot(DataAscending,aes(x=stateName,y=TotMurders)) + geom_col(aes(fill=percentOver18)) + theme(axis.text.x=element_text(angle=90,hjust=1)) + ggtitle("Total Murders")
+BarMurdersOvrET<-ggplot(MergedData,aes(x=reorder(stateName,TotMurders),y=TotMurders)) + geom_col(aes(fill=percentOver18)) + theme(axis.text.x=element_text(angle=90,hjust=1)) + ggtitle("Total Murders")
 
 
 
