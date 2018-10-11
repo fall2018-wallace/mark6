f24ed6a6266d0c5d77ddc5f1542afe73a6d70e9e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:42:03 -0400

    Saving Plotter.R

--- Plotter.R
+++ Plotter.R
@@ -1,2 +1,17 @@
 
+#Step B: Explore the Data – Understanding distributions
+#4)	Create a histogram using GGPLOT for the population and a different histogram for the murder rate
+
 MergedData
+
+options(scipen=999)
+ggplot(MergedData,aes(x=population))+geom_histogram(bins = 50, color = "black", fill = "red")
+
+ggplot(MergedData,aes(x=Murder))+geom_histogram(bins = 50, color = "black", fill = "green")
+
+ggplot(MergedData,aes(x=Assault))+geom_histogram(bins = 50, color = "black", fill = "red")
+
+ggplot(MergedData,aes(x=UrbanPop))+geom_histogram(bins = 50, color = "green", fill = "green")
+
+ggplot(MergedData,aes(x=Rape))+geom_histogram(bins = 50, color = "yellow", fill = "yellow")
+
