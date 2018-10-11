f7d37027566d33d5b89d4afa52111c24bda9233a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:02:32 -0400

    Saving Box_Plotter.R

--- Box_Plotter.R
+++ Box_Plotter.R
@@ -5,3 +5,4 @@ options(scipen=999)
 Box_Pop<-ggplot(MergedData,aes(x=factor(0),y=population))+geom_boxplot()
 Box_Mur<-ggplot(MergedData,aes(x=factor(0),y=Murder))+geom_boxplot()
 
+#6)	Create a block comment explaining which visualization (boxplot or histogram) you thought was more helpful (explain why)
