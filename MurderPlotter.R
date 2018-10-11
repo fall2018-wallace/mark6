2ebe95ec3e8c02aa988f9aaac341743be5ed4da6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:29:50 -0400

    Saving MurderPlotter.R

--- MurderPlotter.R
+++ MurderPlotter.R
@@ -1,9 +1,11 @@
 
 #7:Calculate the number of murders per state
+library(ggplot2)
+# rate provided was per 100,1000. Hence below two lines determine total murders based on rate and population
 x=MergedData$population*MergedData$Murder
 NoOfMurders=x/100000 
 NoOfMurders
 MergedData$TotMurders<-NoOfMurders
 
 #8:Generate a bar chart, with the number of murders per state
-ggplot(MergedData,aes(x=stateName,y=TotMurders)) + geom_col()
+BarMurders<-ggplot(MergedData,aes(x=stateName,y=TotMurders)) + geom_col()
