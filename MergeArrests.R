ab8aa40d75593a452b2d8e5be5bda3716d363fe3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:31:45 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -11,3 +11,6 @@ colnames(ArrestsData)=c("stateName") #Giving name to the new column
 ArrestsData
 
 #3)	Create a merged dataframe -- with the attributes from both dataframes
+MergedData=merge(ArrestsData,newframe,by="stateName") #merging the two dataframes on concolumn name "stateName"
+colnames(MergedData)=c("stateName","Murder","Assault","UrbanPop","Rape","population","popOver18","percentOver18")
+MergedData
