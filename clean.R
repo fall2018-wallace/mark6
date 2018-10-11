24785ed892b6ced7b638cb26fb8a001d988c70ce
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:21:26 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -2,7 +2,7 @@
 # <- read.csv("scprc_est_2017_18_pop_res.csv")
 #dfStates
 
-
+# 1)	Read in the census dataset (using the function created in HW 3)
 readStates <- function(states)
 {
         states <- states[-1,]
