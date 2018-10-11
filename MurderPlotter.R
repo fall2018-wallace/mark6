096ad6ab9c7e36f8d085b5b22729c13f8f5fdbdd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:21:12 -0400

    Saving MurderPlotter.R

--- MurderPlotter.R
+++ MurderPlotter.R
@@ -1,6 +1,6 @@
 
 #7:Calculate the number of murders per state
-x=MergedData$population*MergedData$Murder
+x=MergedData$population/MergedData$Murder
 NoOfMurders=x/100000 # rate provided was per 100,1000
 NoOfMurders
 MergedData
