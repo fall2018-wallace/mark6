fa73328bc8791d1305bc66408258ab891a2cad60
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:26:02 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -7,7 +7,6 @@ ArrestsData <- USArrests
 
 
 ArrestsData=cbind(rownames(ArrestsData),ArrestsData) #Combine sequence of dataframe by rows or columns
-ArrestsData#the above function copies the rownames and places it as a column in the existing dataframe 
 colnames(x)=c("stateName") #Giving name to the new column
 View(x)
 
